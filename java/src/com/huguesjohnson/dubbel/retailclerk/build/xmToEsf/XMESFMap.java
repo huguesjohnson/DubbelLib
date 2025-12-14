88a97e7b4bfd6a231d4ff0e3e4213dcbaac9711b
Hugues Johnson <hugues@huguesjohnson.com>
2025-12-14 14:46:45 -0600

    Update XMESFMap.java
    Just adding comments to document what the various fields mean

--- java/src/com/huguesjohnson/dubbel/retailclerk/build/xmToEsf/XMESFMap.java
+++ java/src/com/huguesjohnson/dubbel/retailclerk/build/xmToEsf/XMESFMap.java
@@ -32,24 +32,25 @@ public class XMESFMap{
 	public final static int DEFAULT_XMSYNC=-1;
 	public final static boolean DEFAULT_NOTE_OFF_PSG=false;
 	
-	public String xmFilePath;
-	public String esfOutputPath;
+	public String xmFilePath;//the path to the xm file being read
+	public String esfOutputPath;//the path to the esf file being created
+	public String label;//could also be called track name
 	public String description;//optional, to make json more readable
-	public ESFType type=ESFType.BGM;
-	public PSGRetriggerVolumeEnvelope psgRetriggerVolumeEnvelope=PSGRetriggerVolumeEnvelope.NEVER;
-	public boolean loop=DEFAULT_LOOP;
-	public int loopPattern=-1;
-	public int loopRow=-1;
-	public int xmSync=DEFAULT_XMSYNC;
-	public boolean ignoreNoteOffPSG=DEFAULT_NOTE_OFF_PSG;
-	public int tempo=DEFAULT_TEMPO;
-	public EchoNoise noiseType=EchoNoise.NONE;
-	public int[] fmChannelMap={-1,-1,-1,-1,-1,-1};
-	public int[] fmVolume={-1,-1,-1,-1,-1,-1};
-	public int[] fmPitch={-1,-1,-1,-1,-1,-1};
-	public int[] psgChannelMap={-1,-1,-1,-1};
-	public int[] psgVolume={-1,-1,-1,-1};
-	public int[] psgPitch={-1,-1,-1,-1};
-	public int pcmChannel=-1;
-	public Map<Integer,Integer> instrumentMap=new HashMap<Integer,Integer>();
+	public ESFType type=ESFType.BGM;//is this background music or a sound effect
+	public PSGRetriggerVolumeEnvelope psgRetriggerVolumeEnvelope=PSGRetriggerVolumeEnvelope.NEVER;//I don't know how this works yet
+	public boolean loop=DEFAULT_LOOP;//simple whether or not this loops
+	public int loopPattern=-1;//if loop==true, this is the pattern index to loop back to
+	public int loopRow=-1;//if loop=true, this is the row in the pattern
+	public int xmSync=DEFAULT_XMSYNC;//I don't know how this works yet
+	public boolean ignoreNoteOffPSG=DEFAULT_NOTE_OFF_PSG;//I don't know how this works yet
+	public int tempo=DEFAULT_TEMPO;//or ticks per second
+	public EchoNoise noiseType=EchoNoise.NONE;//used for PSG audio with noise
+	public int[] fmChannelMap={-1,-1,-1,-1,-1,-1};//map channels in the xm file to console fm channels
+	public int[] fmVolume={-1,-1,-1,-1,-1,-1};//volume for each fm channel
+	public int[] fmPitch={-1,-1,-1,-1,-1,-1};//pitch for each fm channel
+	public int[] psgChannelMap={-1,-1,-1,-1};//map channels in the xm file to console psg channels
+	public int[] psgVolume={-1,-1,-1,-1};//volume for each psg channel
+	public int[] psgPitch={-1,-1,-1,-1};//pitch for each psg channel
+	public int pcmChannel=-1;//map a channel in the xm file to the pcm channel
+	public Map<Integer,Integer> instrumentMap=new HashMap<Integer,Integer>();//xm instrument to echo instrument
 }
