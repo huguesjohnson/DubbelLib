cef2bdf090062b0eb14daa513edfd30ed69b4be2
Hugues Johnson <hugues@huguesjohnson.com>
2026-02-08 13:52:40 -0600

    RLE encoding support for 8x8 tiles

--- java/src/com/huguesjohnson/dubbel/retailclerk/build/objects/Tile8x8.java
+++ java/src/com/huguesjohnson/dubbel/retailclerk/build/objects/Tile8x8.java
@@ -4,7 +4,10 @@ package com.huguesjohnson.dubbel.retailclerk.build.objects;
 
 public class Tile8x8{
 	public final static String newLine=System.lineSeparator();
+	public final static String asmLineStart="\tdc.l\t$";
+	public final static String rleLineStart="\tdc.b\t$";
 
+	
 	public int[][] pixels;
 	
 	public Tile8x8(){this.pixels=new int[8][8];}
@@ -12,12 +15,45 @@ public class Tile8x8{
 	public String toAsmLines(){
 		StringBuffer lines=new StringBuffer();
 		for(int x=0;x<8;x++){
-			lines.append("\tdc.l\t$");
+			lines.append(asmLineStart);
 			for(int y=0;y<8;y++){
 				lines.append(Integer.toHexString(this.pixels[x][y]).toUpperCase());
 			}
 			lines.append(newLine);
 		}
+		return(lines.toString());
+	}
+	
+	public String toRLELines(){
+		StringBuffer lines=new StringBuffer();
+		int currentPixel=this.pixels[0][0];
+		int consecutiveCount=-1;//start at -1 because it will be incremented immediately
+		lines.append(rleLineStart);
+		int x=0;
+		while(x<8){
+			int y=0;
+			while(y<8){
+				if(this.pixels[x][y]!=currentPixel){//pixel has changed
+					lines.append(Integer.toHexString(consecutiveCount).toUpperCase());
+					lines.append(",");
+					lines.append(Integer.toHexString(currentPixel).toUpperCase());
+					lines.append(newLine);
+					currentPixel=this.pixels[x][y];
+					consecutiveCount=0;
+					//start next line
+					lines.append(rleLineStart);
+				}else{
+					//increment counter
+					consecutiveCount++;
+				}
+				y++;
+			}
+			x++;
+		}
+		//write the final line
+		lines.append(Integer.toHexString(consecutiveCount).toUpperCase());
+		lines.append(",");
+		lines.append(Integer.toHexString(currentPixel).toUpperCase());			
 		return(lines.toString());
 	}
 
--- java/src/com/huguesjohnson/dubbel/retailclerk/build/objects/test/TestTile8x8.java
+++ java/src/com/huguesjohnson/dubbel/retailclerk/build/objects/test/TestTile8x8.java
@@ -37,6 +37,87 @@ class TestTile8x8{
 	}
 	
 	@Test
+	void testToRLELines(){
+		Tile8x8 tile0=new Tile8x8();
+		Tile8x8 tile1=new Tile8x8();
+		for(int r=0;r<8;r++){
+			for(int c=0;c<8;c++){
+				tile0.pixels[r][c]=0;
+				tile1.pixels[r][c]=r+c;
+			}
+		}		
+		String[] lines=tile0.toRLELines().split(Tile8x8.newLine);
+		assertEquals(1,lines.length);
+		assertTrue(lines[0].indexOf("$3F,0")>0);
+		lines=tile1.toRLELines().split(Tile8x8.newLine);
+		assertEquals(64,lines.length);
+		assertTrue(lines[0].indexOf("$0,0")>0);
+		assertTrue(lines[1].indexOf("$0,1")>0);
+		assertTrue(lines[2].indexOf("$0,2")>0);
+		assertTrue(lines[3].indexOf("$0,3")>0);
+		assertTrue(lines[4].indexOf("$0,4")>0);
+		assertTrue(lines[5].indexOf("$0,5")>0);
+		assertTrue(lines[6].indexOf("$0,6")>0);
+		assertTrue(lines[7].indexOf("$0,7")>0);
+		assertTrue(lines[8].indexOf("$0,1")>0);
+		assertTrue(lines[9].indexOf("$0,2")>0);
+		assertTrue(lines[10].indexOf("$0,3")>0);
+		assertTrue(lines[11].indexOf("$0,4")>0);
+		assertTrue(lines[12].indexOf("$0,5")>0);
+		assertTrue(lines[13].indexOf("$0,6")>0);
+		assertTrue(lines[14].indexOf("$0,7")>0);
+		assertTrue(lines[15].indexOf("$0,8")>0);
+		assertTrue(lines[16].indexOf("$0,2")>0);
+		assertTrue(lines[17].indexOf("$0,3")>0);
+		assertTrue(lines[18].indexOf("$0,4")>0);
+		assertTrue(lines[19].indexOf("$0,5")>0);
+		assertTrue(lines[20].indexOf("$0,6")>0);
+		assertTrue(lines[21].indexOf("$0,7")>0);
+		assertTrue(lines[22].indexOf("$0,8")>0);
+		assertTrue(lines[23].indexOf("$0,9")>0);
+		assertTrue(lines[24].indexOf("$0,3")>0);
+		assertTrue(lines[25].indexOf("$0,4")>0);
+		assertTrue(lines[26].indexOf("$0,5")>0);
+		assertTrue(lines[27].indexOf("$0,6")>0);
+		assertTrue(lines[28].indexOf("$0,7")>0);
+		assertTrue(lines[29].indexOf("$0,8")>0);
+		assertTrue(lines[30].indexOf("$0,9")>0);
+		assertTrue(lines[31].indexOf("$0,A")>0);
+		assertTrue(lines[32].indexOf("$0,4")>0);
+		assertTrue(lines[33].indexOf("$0,5")>0);
+		assertTrue(lines[34].indexOf("$0,6")>0);
+		assertTrue(lines[35].indexOf("$0,7")>0);
+		assertTrue(lines[36].indexOf("$0,8")>0);
+		assertTrue(lines[37].indexOf("$0,9")>0);
+		assertTrue(lines[38].indexOf("$0,A")>0);
+		assertTrue(lines[39].indexOf("$0,B")>0);
+		assertTrue(lines[40].indexOf("$0,5")>0);
+		assertTrue(lines[41].indexOf("$0,6")>0);
+		assertTrue(lines[42].indexOf("$0,7")>0);
+		assertTrue(lines[43].indexOf("$0,8")>0);
+		assertTrue(lines[44].indexOf("$0,9")>0);
+		assertTrue(lines[45].indexOf("$0,A")>0);
+		assertTrue(lines[46].indexOf("$0,B")>0);
+		assertTrue(lines[47].indexOf("$0,C")>0);
+		assertTrue(lines[48].indexOf("$0,6")>0);
+		assertTrue(lines[49].indexOf("$0,7")>0);
+		assertTrue(lines[50].indexOf("$0,8")>0);
+		assertTrue(lines[51].indexOf("$0,9")>0);
+		assertTrue(lines[52].indexOf("$0,A")>0);
+		assertTrue(lines[53].indexOf("$0,B")>0);
+		assertTrue(lines[54].indexOf("$0,C")>0);
+		assertTrue(lines[55].indexOf("$0,D")>0);
+		assertTrue(lines[56].indexOf("$0,7")>0);
+		assertTrue(lines[57].indexOf("$0,8")>0);
+		assertTrue(lines[58].indexOf("$0,9")>0);
+		assertTrue(lines[59].indexOf("$0,A")>0);
+		assertTrue(lines[60].indexOf("$0,B")>0);
+		assertTrue(lines[61].indexOf("$0,C")>0);
+		assertTrue(lines[62].indexOf("$0,D")>0);
+		assertTrue(lines[63].indexOf("$0,E")>0);
+	}	
+	
+	@Test
 	void testEquals(){
 		Tile8x8 tile0=new Tile8x8();
 		Tile8x8 tile1=new Tile8x8();
